755621a16ceaef3ed360882dbd970718b9a78894
afriklabprojet <afriklabprojet@gmail.com>
2026-06-07 15:27:05 +0200

    feat: inject PromoBanner between header and main content in shop layout

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -4,6 +4,7 @@ import CartSidebar from '@/components/CartSidebar';
 import WhatsAppFAB from '@/components/WhatsAppFAB';
 import BottomNav from '@/components/BottomNav';
 import PWAInstaller from '@/components/PWAInstaller';
+import PromoBanner from '@/components/PromoBanner';
 import { CartProvider } from '@/lib/cart-context';
 import { getSiteSettings } from '@/lib/site-settings';
 import { SiteSettingsProvider } from '@/lib/site-settings-context';
@@ -18,6 +19,7 @@ export default async function ShopLayout({ children }: Readonly<{ children: Reac
     <SiteSettingsProvider value={siteSettings}>
       <CartProvider>
         <Header navCategories={navCategories} />
+        <PromoBanner />
         <main className="flex-1 pb-16 lg:pb-0">
           {children}
         </main>
